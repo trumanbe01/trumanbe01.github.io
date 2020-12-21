fac16f51eba89ac94ef42139a168a17ecf8d9599
Truman Be <trumanbe01@gmail.com>
2020-12-21 14:56:00 -0500

    Initial commit to set up files

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,18 @@
+export const MenuItems = [
+    {
+        title: 'About',
+        className: 'about-section'
+    },
+    {
+        title: 'Portfolio',
+        className: 'portfolio-section'
+    },
+    {
+        title: 'Activities',
+        className: 'activities-section'
+    },
+    {
+        title: 'contact',
+        className: 'contact-section'
+    },
+]
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { MenuItems } from './MenuItems';
+
+class Navbar extends Component {
+    render() {
+        return(
+            <h1>navbar</h1>
+        )
+    }
+}
+
+export default Navbar
