1f1a1a27aa8c2b717c35bc174f6442afd7f53884
Truman Be <trumanbe01@gmail.com>
2020-12-27 12:37:24 -0500

    Improved the general layout of the tabs done by styling; more styling needed

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -7,9 +7,8 @@ class Project extends Component {
             <div className={this.props.direction + 'Project'}>
                 <img className="project-img" src={this.props.image} />
                 <div className="project-text">
-                    <h1 className="project-name">{this.props.name}</h1>
+                    <a className="project-name" href={this.props.repo}>{this.props.name}</a>
                     <p className="project-techstack">Tech stack: {this.props.tech}</p>
-                    <a className="project-repo" href={this.props.repo} target="_blank">Repo link</a>
                     <p className="project-desc">{this.props.desc}</p>
                 </div>
             </div>
