984fdd0d1651c6de89e1349791d75628512c5b03
Truman Be <trumanbe01@gmail.com>
2020-12-27 23:53:26 -0500

    Added template code for the Activity component

---
+++ src/components/Activity/Activity.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { ActivityItems } from './ActivityItems'
+import 'Activity.css';
+
+class Activity extends Component {
+    render() {
+        return(
+
+        )
+    }
+}
+
+export default Activity
---
+++ src/components/Activity/ActivityItems.js
@@ -0,0 +1,8 @@
+export default ActivityItems = [
+    {
+        name: 'Head TA',
+        date: 'September 2020 - December 2020',
+        desc: 'Head Teaching Assistant for Discrete Mathematics I at Queen\'s University',
+        image: 'https://media-exp1.licdn.com/dms/image/C560BAQHvC1jcUTqmfQ/company-logo_200_200/0/1563980244871?e=2159024400&v=beta&t=lMGCuN-JYPxUUUjts962JzJXzisrgBPohi0KJ02TH0U'
+    },
+]
