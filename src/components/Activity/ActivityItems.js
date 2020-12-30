66b33ffd95abdcd8a7fade450e719c06bb7314a0
Truman Be <trumanbe01@gmail.com>
2020-12-30 12:07:32 -0500

    Modified activity items, as well as taking out descriptions

--- src/components/Activity/ActivityItems.js
+++ src/components/Activity/ActivityItems.js
@@ -1,16 +1,56 @@
 export const ActivityItems = [
     {
-        role: 'Head TA',
-        org: 'Queen\'s University School of Computing',
-        date: 'September 2020 - Present',
-        desc: 'Head Teaching Assistant for Discrete Mathematics I at Queen\'s University',
-        image: 'https://media-exp1.licdn.com/dms/image/C560BAQHvC1jcUTqmfQ/company-logo_200_200/0/1563980244871?e=2159024400&v=beta&t=lMGCuN-JYPxUUUjts962JzJXzisrgBPohi0KJ02TH0U'
+        role: 'Community Outreach Subcommittee Coordinator',
+        org: 'Queen\'s CODE',
+        date: 'January 2021 - Present',
+        image: '#' 
+    },
+    {
+        role: 'Peer Tutor',
+        org: 'Queen\'s University Arts and Science Undergraduate Society',
+        date: 'October 2020 - Present',
+        image: 'https://static1.squarespace.com/static/5ecff665f5a30a31256a2efe/t/5ede4a329ae1433b3ad835ac/1606322231241/?format=1500w'
+    },
+    {
+        role: 'Private Tutor',
+        org: 'Self-employed',
+        date: 'October 2020 - Present',
+        image: '#'
     },
     {
         role: 'Junior Developer',
-        org: 'QTMA',
+        org: 'Queen\'s Technology and Media Association (QTMA)',
         date: 'May 2020 - Present',
-        desc: 'Junior Developer at Queen\'s Technology and Media Association (QTMA)',
         image: 'https://cdn-images-1.medium.com/max/1200/1*h9ONFFFHOuJJBKIISjQA5w.png'
+    },
+    {
+        role: 'General Member (Electrical Division)',
+        org: 'Queen\'s Soft Robotics',
+        date: 'October 2019 - Present',
+        image: 'https://media-exp1.licdn.com/dms/image/C560BAQEffyTGjbrphg/company-logo_200_200/0?e=2159024400&v=beta&t=l67VhDBUEXpJeXX8Sfp-3YPGK3E65RZdJ979HchaXK0'
+    },
+    {
+        role: 'Head Teaching Assistant (Discrete Mathematics I)',
+        org: 'Queen\'s University School of Computing',
+        date: 'September 2020 - December 2020',
+        image: 'https://media-exp1.licdn.com/dms/image/C560BAQHvC1jcUTqmfQ/company-logo_200_200/0/1563980244871?e=2159024400&v=beta&t=lMGCuN-JYPxUUUjts962JzJXzisrgBPohi0KJ02TH0U'
+    },
+    {
+        role: 'Teaching Assistant (Discrete Mathematics I)',
+        org: 'Queen\'s University School of Computing',
+        date: 'May 2020 - August 2020',
+        image: 'https://media-exp1.licdn.com/dms/image/C560BAQHvC1jcUTqmfQ/company-logo_200_200/0/1563980244871?e=2159024400&v=beta&t=lMGCuN-JYPxUUUjts962JzJXzisrgBPohi0KJ02TH0U'
+    },
+    {
+        role: 'Tutoring Assistant',
+        org: 'Kumon Math and Reading Center',
+        date: 'June 2018 - August 2019',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR5Q6UJlA4wUR7rMf7i6I8JZWb2_ZWdeg0wA&usqp=CAU'
+    },
+    {
+        role: 'Team Lead/Programmer',
+        org: 'Castlebrooke Computer Engineering Teachers\' Association (CETA) Robotics Team',
+        date: 'September 2018 - April 2019',
+        image: '#'
     }
 ]
