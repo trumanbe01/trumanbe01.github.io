48559ebd6d1b1c56be5fc04f7e5da7ec8e9ef350
Truman Be <trumanbe01@gmail.com>
2020-12-30 11:53:20 -0500

    Modified the items that will be presented in the projects section

--- src/components/Project/ProjectItems.js
+++ src/components/Project/ProjectItems.js
@@ -1,16 +1,23 @@
 export const ProjectItems = [
     {
         name: 'Swype',
-        tech: ['React Native', 'Yelp Fusion API', 'Google Cloud APIs'],
-        desc: "A simplistic mobile app that retrieves restaurants according to the user's preferences, and uses a minimalist UI to help narrow options for the user. Swype was developed using React Native at Hack Western, a 36-hour hackathon.",
+        tech: ['React Native', 'Yelp Fusion API', 'Google Distance Matrix API', 'Google Geolocation API'],
+        desc: "A simplistic mobile app that retrieves restaurants according to the user's preferences, and uses a minimalist UI to help narrow options for the user. Swype was developed at Hack Western, a 36-hour hackathon hosted by the University of Western Ontario.",
         image: 'https://raw.githubusercontent.com/trumanbe01/swype/main/assets/swypelogored.png',
         repo: 'https://github.com/trumanbe01/swype'
     },
     {
         name: 'Journey',
-        tech: ['React', 'Firebase (Firestore)', 'Google Natural Language API'],
-        desc: "A mental health and wellness web journal application, built with React, Express.js, and Firebase. Awarded 'Most Innovative Project' at hackHer, a 24hr hackathon hosted by Queen's Women in Computing.",
+        tech: ['React', 'Express.js', 'Firebase (Firestore)', 'Google Natural Language API'],
+        desc: "A mental health and wellness web journal application that analyzes the sentiment of journal entries by the user. Awarded 'Most Innovative Project' at hackHer, a 24hr hackathon hosted by Queen's Women in Computing.",
         image: 'https://raw.githubusercontent.com/mkevinq/journey/main/src/images/journey-logo.png',
         repo: 'https://github.com/trumanbe01/journey'
-    }
+    },
+    {
+        name: 'TanglePath',
+        tech: ['HTML', 'Javascript', 'Google Maps Geocoding API'],
+        desc: "A web application that allows the user to chart their journey, listing places they've lived and/or visited and plotting a visual unique to their journey. TanglePath was presented at QHacks, a 36-hour hackathon hosted by Queen's University.",
+        image: '#',
+        repo: 'https://github.com/trumanbe01/tanglepath'
+    }   
 ]
