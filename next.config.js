c5f9de54d07931b8ef10171505f30cc0ae75fe48
Truman <trumanbe01@gmail.com>
2024-01-05 20:21:08 -0500

    Disable server-based image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig}*/
 const nextConfig = {
-    output: 'export'
-}
+  /**
+   * Fixing Next.js config for Github Pages
+   * https://github.com/gregrickaby/nextjs-github-pages
+   */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
